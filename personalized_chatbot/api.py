1b8f9355b24e7868081467a942da4cc178beb715
mustafatawab <engrmustafatawab05@gmail.com>
2025-08-08 20:22:17 +0500

    added cors origin

--- personalized_chatbot/api.py
+++ personalized_chatbot/api.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI, Body, Path, Query, HTTPException
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 from main import run_agent
 
+app: FastAPI = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or restrict to your frontend domain
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Chat(BaseModel):
     input: str
 
-app: FastAPI = FastAPI()
 
 
 @app.get("/")
